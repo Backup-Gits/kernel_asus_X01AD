8765e889e3777d5bd4db7c8573b45c4e6a3aa4e3
Lau <laststandrighthere@gmail.com>
2020-05-20 03:20:55 +0800

    usb: uvc: Omit debug/tracing drivers when debug is disabled
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -1854,7 +1854,9 @@ static void uvc_unregister_video(struct uvc_device *dev)
 
 		video_unregister_device(&stream->vdev);
 
+#ifdef CONFIG_DEBUG_FS
 		uvc_debugfs_cleanup_stream(stream);
+#endif
 	}
 
 	/* Decrement the stream count and call uvc_delete explicitly if there
@@ -1885,7 +1887,9 @@ static int uvc_register_video(struct uvc_device *dev,
 		return ret;
 	}
 
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_init_stream(stream);
+#endif
 
 	/* Register the device with V4L. */
 
@@ -2703,11 +2707,15 @@ static int __init uvc_init(void)
 {
 	int ret;
 
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_init();
+#endif
 
 	ret = usb_register(&uvc_driver.driver);
 	if (ret < 0) {
+#ifdef CONFIG_DEBUG_FS
 		uvc_debugfs_cleanup();
+#endif
 		return ret;
 	}
 
@@ -2718,7 +2726,9 @@ static int __init uvc_init(void)
 static void __exit uvc_cleanup(void)
 {
 	usb_deregister(&uvc_driver.driver);
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_cleanup();
+#endif
 }
 
 module_init(uvc_init);
